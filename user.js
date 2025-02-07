2d0a7e13b56273316555ea9d63287f081a3fadd0
Rage1703 <mehmetgorgulu1703@gmail.com>
2025-02-08 02:05:23 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,8 @@
-/**TEST BETİĞİ */
+// ==UserScript==
+// @name         Müdavim Takip Scripti
+// @version      1.0
+// @description  Otomatik doldurma betiği
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/kullanici_adiniz/depo_adi/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/kullanici_adiniz/depo_adi/main/script.user.js
+// ==/UserScript==
