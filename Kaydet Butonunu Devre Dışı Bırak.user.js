9f7b4bbb39d9ade62e09fe3a2f049b0e22627b25
Rage1703 <mehmetgorgulu1703@gmail.com>
2025-03-12 08:36:13 +0300

    12.03.2025

--- Kaydet Butonunu Devre Dışı Bırak.user.js
+++ Kaydet Butonunu Devre Dışı Bırak.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kaydet Butonunu Devre Dışı Bırak
-// @version      1.1
-// @description  Açıklama alanı boşken kaydet butonunu devre dışı bırakır.
+// @version      1.3
+// @description  Açıklama alanı boşken kaydet butonunu devre dışı bırakır, ancak başka kısıtlamalara müdahale etmez.
 // @author       Rage1703
 // @match        https://sabim.sonitel.io/custom/sabim?MobileNumber=*
 // @grant        none
@@ -16,11 +16,17 @@
 
         if (textarea && button) {
             if (textarea.value.trim() === "") {
+                button.setAttribute("data-textarea-empty", "true");
                 button.setAttribute("disabled", "true");
                 button.classList.add("mat-button-disabled");
             } else {
-                button.removeAttribute("disabled");
-                button.classList.remove("mat-button-disabled");
+                button.removeAttribute("data-textarea-empty");
+
+                // Eğer başka bir engel yoksa butonu aktif hale getir
+                if (button.hasAttribute("disabled") && button.getAttribute("data-textarea-empty") !== "true") {
+                    button.removeAttribute("disabled");
+                    button.classList.remove("mat-button-disabled");
+                }
             }
         }
     }
