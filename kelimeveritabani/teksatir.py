f0d1d667c3902a605360a1ebf5041db3b66a860c
Rage1703 <mehmetgorgulu1703@gmail.com>
2025-03-20 00:24:58 +0300

    20.03.2025

--- kelimeveritabani/teksatir.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# JSON dosyasını oku
-with open("kelimeler.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-# JSON verisini tek satıra sıkıştırılmış olarak kaydet
-with open("kelimeler_min.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, separators=(",", ":"), ensure_ascii=False)
